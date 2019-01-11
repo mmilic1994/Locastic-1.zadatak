adcdb582ab627b4a4efd1281b021e5fab8fc4ea7
mmilic1994 <mmilic1994@gmail.com>
2019-01-11 12:45:17 +0100

    I changed the root folder structure and I started building the site by applying mobile-first approach

---
+++ dist/js/vmc.simple.slide.js
@@ -0,0 +1,41 @@
+
+$('#vmc-slide').vmcSimpleSlide({
+
+  // width/height
+  width: 'auto',
+  height: 'auto',
+
+  // min width
+  minWidth: 0,
+
+  // min height
+  minHeight: 0,
+
+  // autoplay
+  auto: true,
+
+  // 'right' or 'left'
+  autoPlayDirection: 'right',
+
+  // duration of animation
+  duration: 4000,
+
+  // animation speed
+  speed: 600,
+
+  // shows navigation buttons
+  showSideButton: true,
+
+  // shows pagination bullets
+  showBottomButton: true,
+
+  // shows image caption
+  showSummary: 'auto',
+
+  // allows HTML content
+  summaryIsHtml: false,
+
+  // pause on hover
+  hoverStop: true
+  
+});
